6f8c4534223d2df7e2bd74b067b2fd49e485aced
tvijay333 <tvijay333@gmail.com>
2021-11-16 15:38:22 +0530

    Update script.js
    Handling negative values and focus after alert

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
-	if (principal==0){
+	if (principal <= 0){
 		alert("Enter a positive number");
+		document.getElementById("principal").focus();
 	}
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
@@ -10,8 +11,11 @@ function compute()
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
 	
-	if (interest !=0){
+	if (interest > 0){
 	document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";
+	}
+	else{
+		document.getElementById("result").innerHTML=''
 	}
 	
 }
